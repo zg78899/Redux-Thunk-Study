36379a9f910d63df2b28b3358ba7bbe0044c51bf
kim jae hun <zg788990@gmail.com>
2020-03-12 21:23:49 +0900

    handleAsyncActions reducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import counter from './counter';
+import posts from './posts';
 //루트 리듀서를 만든다.
 //이후의 작업은 프로젝트에 리덕스를 적용해준다.
 //index.js에서 Provider을 불러온다.
 
 const rootReducer=combineReducers({
-  counter
+  counter,
+  posts
 });
 
 export default rootReducer;
