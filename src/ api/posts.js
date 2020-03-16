c902a612cbcbd333af5316ab6d9b7a9e6e5d12ac
kim jae hun <zg788990@gmail.com>
2020-03-16 20:22:11 +0900

    package.json add proxy port 3000

--- src/ api/posts.js
+++ src/ api/posts.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const getPosts =async()=>{
-  const response =await axios.get('http://localhost:4000/posts')
+  const response =await axios.get('/posts')
   //응답된 데이터를 반환하겠다.
   return response.data;
 }
 
 export const getPostById =async (id)=>{
-const response = await axios.get(`http://localhost:4000/posts/${id}`);
+const response = await axios.get(`/posts/${id}`);
 return response.data;
 }
